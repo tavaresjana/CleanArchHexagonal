68ebbd730e964d4641cdd42216b46948d1c65019
Janaina <tjanaina92@gmail.com>
2023-08-23 13:57:52 -0300

    Configurações do Kafka

---
+++ hexagonal/src/main/java/com/jana/hexagonal/adapters/in/consumer/message/CustomerMessage.java
@@ -0,0 +1,21 @@
+package com.jana.hexagonal.adapters.in.consumer.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.kafka.common.protocol.types.Field;
+
+import javax.swing.text.StyledEditorKit;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerMessage {
+    private String id;
+    private String name;
+    private String zipCode;
+    private String cpf;
+    private Boolean isValidCpf;
+    /*Então a ideia é a gente enviar por tópico do Kafka o CPF e a aplicação fictícia simula a validação de CPF,
+retornando o pedido do cliente, o nome e o CEP junto com o CPF também falando se está válido ou não.*/
+}
